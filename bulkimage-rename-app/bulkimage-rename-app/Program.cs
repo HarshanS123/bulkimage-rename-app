e204fb675732be1d383d29041026995104d3e46f
Harshan Somarathna <59196911+Star-harshan@users.noreply.github.com>
2021-03-12 19:27:44 +0530

    Add IO and complete method

--- bulkimage-rename-app/bulkimage-rename-app/Program.cs
+++ bulkimage-rename-app/bulkimage-rename-app/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,41 @@ namespace bulkimage_rename_app
 
             Console.WriteLine("Please Enter New Name");
             string text = Console.ReadLine();
+            try
+            {
+                if (!spath.Equals("") && spath != null)
+                {
+                    if (!savepath.Equals("") && savepath != null)
+                    {
+                        if (!text.Equals("") && text != null)
+                        {
+                            DirectoryInfo d = new DirectoryInfo(spath);
+                            FileInfo[] Files = d.GetFiles("*.jpg");
+
+                            foreach (FileInfo file in Files)
+                            {
+                                var a = file.FullName.ToString().Split('\\').Last();
+                                var newname = text.Trim() + a;
+                                var path = savepath + '\\' + newname;
+                                File.Move(file.FullName, path);
+                            }
+
+
+                        }
+
+                    }
+
+                }
+
+                Console.WriteLine("........");
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
